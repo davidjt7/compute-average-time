fb51851b0f5b45246b23822edf89dfa4a536007b
David Joseph Thomas <djt.in7@gmail.com>
2020-09-23 19:55:54 +0530

    Hotfix

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,2 @@
-import { type } from 'os';
 import {UserSortKey, CountrySortKey} from './types';
 export function analyzeTasks(input: string, userSortKey?: UserSortKey, countrySortKey?: CountrySortKey): string;
